db4d22531ff37a70511353fbca07b2e82db50d81
Srikar Naramsetty <srikarnaramsetty@gmail.com>
2020-05-17 23:30:01 +0530

    Renaming files

--- installclamavcron.sh
+++ installclamavcron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /home/clamav
 cd /home/clamav
-curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/compliance/clamavscan.sh
+curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/compliance/clamavrunscan.sh
 chmod 0755 clamavscan.sh
 ln /home/clamav/clamavscan.sh /etc/cron.hourly/clamscan_hourly
 exit 0
