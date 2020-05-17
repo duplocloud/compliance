9721931b944d47a99478d2c002a213fda71d1a9f
Srikar Naramsetty <srikarnaramsetty@gmail.com>
2020-05-17 22:17:55 +0530

    Clamav scripts
    Clamav scripts

---
+++ ClamAVScan.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+LOGFILE="/var/log/clamav/clamav-$(date +'%Y-%m-%d').log";
+DIRTOSCAN="/home";
+
+# Update ClamAV database
+echo "Looking for ClamAV database updates...";
+freshclam --quiet;
+
+TODAY=$(date +%u);
+
+if [ "$TODAY" == "6" ];then
+ echo "Starting a full weekend scan.";
+
+ # be nice to others while scanning the entire root
+ nice -n5 clamscan -ri / --exclude-dir=/sys/ -l "$LOGFILE" --stdout| logger -i -t clamd;
+else
+ DIRSIZE=$(du -sh "$DIRTOSCAN" 2>/dev/null | cut -f1);
+
+ echo "Starting a daily scan of "$DIRTOSCAN" directory.
+ Amount of data to be scanned is "$DIRSIZE".";
+
+ clamscan -ri "$DIRTOSCAN" -l "$LOGFILE" --stdout| logger -i -t clamd;
+fi
+exit 0
