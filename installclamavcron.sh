a5317fefcc9cba157ec6601377d96f3d3097fd7d
Srikar Naramsetty <srikarnaramsetty@gmail.com>
2020-05-18 01:07:09 +0530

    wget fix
    wget fix

--- installclamavcron.sh
+++ installclamavcron.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 mkdir -p /home/clamav
 cd /home/clamav
-wget https://raw.githubusercontent.com/duplocloud/compliance/master/clamavrunscan.sh
+wget -O clamavrunscan.sh https://raw.githubusercontent.com/duplocloud/compliance/master/clamavrunscan.sh
 chmod 0755 clamavrunscan.sh
+unlink /etc/cron.hourly/clamscan_hourly
 ln /home/clamav/clamavrunscan.sh /etc/cron.hourly/clamscan_v1_hourly
 exit 0
