a7f5e2e2a510d95806c269deed5b8dd91947f538
Srikar Naramsetty <srikar@duplocloud.net>
2021-02-08 18:46:41 +0530

    ClamAV scan weekly bug fix

--- installclamavcron.sh
+++ installclamavcron.sh
@@ -8,5 +8,5 @@ chmod 0755 clamavfullscan.sh
 unlink /etc/cron.daily/clamscan_daily
 unlink /etc/cron.weekly/clamscan_weekly
 ln /home/clamav/clamavnormalscan.sh /etc/cron.daily/clamscan_daily
-ln /home/clamav/clamavfullscan.sh /etc/cron.daily/clamscan_weekly
+ln /home/clamav/clamavfullscan.sh /etc/cron.weekly/clamscan_weekly
 exit 0
