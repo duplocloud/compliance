11691cd1f75d67bcfa522bf7bfc3a9e92d874326
Srikar Naramsetty <srikar@duplocloud.net>
2021-02-08 22:33:23 +0530

    unlinking existing hourly scan

--- installclamavcron.sh
+++ installclamavcron.sh
@@ -5,6 +5,7 @@ wget -O clamavnormalscan.sh https://raw.githubusercontent.com/duplocloud/complia
 wget -O clamavfullscan.sh https://raw.githubusercontent.com/duplocloud/compliance/master/clamavfullscan.sh
 chmod 0755 clamavnormalscan.sh
 chmod 0755 clamavfullscan.sh
+unlink /etc/cron.hourly/clamscan_v1_hourly
 unlink /etc/cron.daily/clamscan_daily
 unlink /etc/cron.weekly/clamscan_weekly
 ln /home/clamav/clamavnormalscan.sh /etc/cron.daily/clamscan_daily
